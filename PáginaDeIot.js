8fc21255e4a6864a8b0b4478eea338c99e71bedc
Nathan Liberatti <nathan.liberatti@gmail.com>
2024-11-27 10:18:54 -0300

    Add files via upload

---
+++ PáginaDeIot.js
@@ -0,0 +1,9 @@
+// Bootstrap JS
+    src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js">
+
+    // Custom JS 
+    
+        function redirect() {
+            window.location.href = 'SitePessoal.HTML';
+        }
+    
